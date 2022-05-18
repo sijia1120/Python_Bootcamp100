662dc1b9e22b6112a39c3a398e5b21b8ad7ef332
Scarlett <98492102+sijia1120@users.noreply.github.com>
2022-05-18 15:44:35 +0100

    Update send_sms.py

--- Day_36/Day_36_me/send_sms.py
+++ Day_36/Day_36_me/send_sms.py
@@ -3,9 +3,9 @@ import os
 from twilio.rest import Client
 
 account_sid ="AC6957718c6e35b4c639b20f9df53cec6c"
-auth_token ="3efc35a2b5c405a5c5206c3faaaad977"
-from_num ="+19282499040"
-to_num ="+4407410499352"
+auth_token ="*********************"
+from_num ="+************"
+to_num ="+***********"
 
 class SendSMS:
     def __init__(self,content):
